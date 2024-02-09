3948e0c33fe1470eef3843fc036b7789ab196882
Eamon <human@eamon.us>
2024-02-09 13:36:49 -0800

    initial commit

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "types"
+
+export const siteConfig: SiteConfig = {
+  name: "Subtle Path LLC",
+  description:
+    "Spatial application developer",
+  url: "https://subtlepath.com",
+  ogImage: "https://subtlepath.com/logo.svg",
+  links: {
+    twitter: "https://twitter.com/subtlepath",
+    github: "https://github.com/subtlepath",
+  },
+}
